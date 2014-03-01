a073052c20825141f556aa293f0a0dc7dc9faf3c
Pawel Niewiadomski <11110000b@gmail.com>
2014-03-01 13:01:45 +0100

    no need for this helper

--- app/helpers/settings_helper.rb
+++ app/helpers/settings_helper.rb
@@ -1,6 +1,4 @@
 module SettingsHelper
-	include ActionView::Helpers::NumberHelper
-
 	def calculate_reminder_at(user)
 		"#{user.reminder_at_h.to_s.rjust(2, '0')}:#{user.reminder_at_m.to_s.rjust(2, '0')}"
 	end
