cb09bb4d3cc7846d411fd1375f9ad6c58facf023
Pawel Niewiadomski <11110000b@gmail.com>
2014-03-01 06:45:39 +0100

    added info in code

--- db/migrate/20140228202647_add_basic_settings_to_user.rb
+++ db/migrate/20140228202647_add_basic_settings_to_user.rb
@@ -1,5 +1,6 @@
 class AddBasicSettingsToUser < ActiveRecord::Migration
   def change
+  	# check how to add an index on array https://coderwall.com/p/sud9ja
   	add_column :users, :timezone, :text, { null: false, default: 'GMT' }
   	add_column :users, :reminder_at_h, :smallint, { null: false, default: 17 }
   	add_column :users, :reminder_at_m, :smallint, { null: false, default: 0 }
