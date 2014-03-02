d3eef422d5119a7fe9e6cd558eea957fd54591a9
Pawel Niewiadomski <11110000b@gmail.com>
2014-03-02 07:40:45 +0100

    ordering in the right order

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -2,6 +2,6 @@ class NotesController < ApplicationController
 	before_filter :require_user
 
 	def index
-		@notes = current_user.notes.order(created_at: :asc)
+		@notes = current_user.notes.order(:created_at)
 	end
 end
