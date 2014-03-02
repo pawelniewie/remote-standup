fb206aee9da4eb0cd345c0dd73068457ca555453
Pawel Niewiadomski <11110000b@gmail.com>
2014-03-02 07:40:03 +0100

    ordering

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -2,6 +2,6 @@ class NotesController < ApplicationController
 	before_filter :require_user
 
 	def index
-		@notes = current_user.notes
+		@notes = current_user.notes.order(created_at: :desc)
 	end
 end
