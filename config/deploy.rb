ca6ccaf85d6abe2946314d105efbb3f8156ac69c
Pawel Niewiadomski <11110000b@gmail.com>
2014-03-09 20:38:59 +0100

    no neet to set env explicitly, it will be set in env

--- config/deploy.rb
+++ config/deploy.rb
@@ -27,8 +27,6 @@ set :default_env, {
   path: '/usr/local/rbenv/shims:/usr/local/rbenv/bin:$PATH'
 }
 
-set(:sidekiq_cmd) { "#{bundle_cmd} exec sidekiq -e production" }
-
 namespace :deploy do
 
   desc 'Restart application'
