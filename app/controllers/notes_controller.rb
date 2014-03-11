d0f25322f2d7509be3d104f224de10915270d3ae
Pawel Niewiadomski <11110000b@gmail.com>
2014-03-11 07:04:07 +0100

    showing all notes by the team (was for the current user)

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -2,6 +2,8 @@ class NotesController < ApplicationController
 	before_filter :authenticate_user!
 
 	def index
-		@notes = current_user.notes.order(created_at: :desc)
+		@notes = Note.where('user_id IN (:users)',
+			:users => User.where('user_id = :admin_id OR admin_id = :admin_id',
+				admin_id: current_user.admin.nil? ? current_user.id : current_user.admin.id).select(:id)).order(created_at: :desc)
 	end
 end
