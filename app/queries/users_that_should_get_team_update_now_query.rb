9e6b8e0aa43eaf5edb22a46e9d585849139a36a2
Pawel Niewiadomski <11110000b@gmail.com>
2014-03-17 07:23:54 +0100

    team updates seem to work

--- app/queries/users_that_should_get_team_update_now_query.rb
+++ app/queries/users_that_should_get_team_update_now_query.rb
@@ -10,7 +10,7 @@ class UsersThatShouldGetTeamUpdateNowQuery
     		 AND EXTRACT(HOUR FROM TIMEZONE(timezone, NOW() + interval '8 hours')) >= reminder_at_h
          AND EXTRACT(MINUTE FROM TIMEZONE(timezone, NOW() + interval '8 hours')) >= reminder_at_m})
     	.find_each do |user|
-        time_range = user.sent_team_update_at..Time.now.midnight
+        time_range = (user.sent_team_update_at.nil? ? Time.now.midnight - 1.day : user.sent_team_update_at)..Time.now.midnight
         if user.team.notes.where(:created_at => time_range).count > 0
           block.call(user)
         end
--- app/workers/email_team_update_worker.rb
+++ app/workers/email_team_update_worker.rb
@@ -3,7 +3,9 @@ class EmailTeamUpdateWorker
 
   def perform(user_id, notes_starting_at)
   	user = User.find(user_id)
-  	notes = Note.where('created_at >= ? AND team_id = ?', notes_starting_at, user.team_id).all
-    UserMailer.team_update_mail(user, notes).deliver
+  	notes = Note.where('created_at >= ? AND team_id = ?', notes_starting_at, user.team_id).to_a
+
+  	logger.info("Queueing team update for #{user.email} with #{notes.size} notes")
+    TeamMailer.team_update_mail(user, notes).deliver
   end
 end
