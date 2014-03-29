a3cac13bc4e82aa9c3c4fe56e911467b266b1be0
Pawel Niewiadomski <11110000b@gmail.com>
2014-03-29 07:12:56 +0100

    changed contact email

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,6 +8,10 @@ module ApplicationHelper
 		'tel:+48-601-789-982'
 	end
 
+	def contact_email
+		'janek@remotestandup.com'
+	end
+
 	def body_class
     [controller_name, action_name].join('-')
   end
