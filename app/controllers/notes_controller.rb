8042ed08a6d3ec35fee3a8e8f3ba943c4d4f8797
Pawel Niewiadomski <11110000b@gmail.com>
2014-04-02 06:31:49 +0200

    it's possible to list notes per discussion

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,7 +1,14 @@
 class NotesController < ApplicationController
 	before_filter :authenticate_user!
+	before_action :set_discussion
 
 	def index
-		@notes = current_user.team.notes.order(created_at: :desc)
+		@notes = @discussion.notes.order(created_at: :desc)
+	end
+
+	private
+
+	def set_discussion
+		@discussion = current_user.team.discussions.find(params[:discussion_id])
 	end
 end
